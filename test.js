9deaf06e4900eab8622ce3a66d06fa8dcc980c74
Badinko007 <72599767+abdul9200@users.noreply.github.com>
2022-01-29 15:18:46 +0000

    ordering files with a json file

--- test.js
+++ test.js
@@ -1,23 +1,51 @@
+function ordonnance(doc){
+  var res=[];
+  for(i=0;i<doc.length;i++){
+    res.push({"type":doc[i]["type"],"path":"/home/pi/Desktop/orderMi/"+doc[i]["name"],"duration":doc[i]["duration"]})
+  }
+  return JSON.stringify(res);
+}
 function getdoc(){
-  ip=require('os').networkInterfaces()["Wi-Fi 3"][1]["address"] ;
-  mac=require('os').networkInterfaces()["Wi-Fi 3"][1]["mac"];
-  res={"IP":ip,
-        "MAC":mac,
-      "id":"112-112-112"}
+  ip=require('os').networkInterfaces()["wlan0"][0]["address"] ;
+  mac=require('os').networkInterfaces()["wlan0"][0]["mac"];
+  const testFolder = '/home/pi/Desktop/orderMi';
+const fs = require('fs');
+var list=[];
+
+fs.readdirSync(testFolder).forEach(file => {
+  list.push(file);
+});
+  res={"id":1,
+	"ip":ip,
+        "mac":mac,
+	"names":list
+      }
 
       return res;
 }
+console.log(getdoc());
 var request = require('request');
 var options = {
   'method': 'POST',
-  'url': 'http://localhost:8080/conge/add',
+  'url': 'http://192.168.43.155:8080/screen/sendMacIp',
   'headers': {
     'Content-Type': 'application/json'
   },
   body: JSON.stringify(getdoc())
 
 };
+setInterval(()=>{
 request(options, function (error, response) {
   if (error) throw new Error(error);
-  console.log(response.body);
-});
+  console.log(JSON.parse(response.body));
+
+  const fs = require('fs');
+  fs.writeFile('orderMi/OrderMedia.json',ordonnance(JSON.parse(response.body)),err=>{
+    if(err){
+      console.error(err)
+      return;
+    }
+    
+    
+    });
+});},5000)
