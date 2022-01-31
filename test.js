ac0d5230eab5baa067929adb94800edffb2c79d1
Badinko007 <72599767+abdul9200@users.noreply.github.com>
2022-01-31 11:34:36 +0000

    final dto sender

--- test.js
+++ test.js
@@ -5,42 +5,52 @@ function ordonnance(doc){
   }
   return JSON.stringify(res);
 }
-function getdoc(){
-  ip=require('os').networkInterfaces()["wlan0"][0]["address"] ;
-  mac=require('os').networkInterfaces()["wlan0"][0]["mac"];
-  const testFolder = '/home/pi/Desktop/orderMi';
-const fs = require('fs');
+function list(){
+  const fs = require('fs');
 var list=[];
-
+const testFolder = '/home/pi/Desktop/orderMi';
 fs.readdirSync(testFolder).forEach(file => {
   list.push(file);
 });
+return list;
+  
+}
+
+function getdoc(){
+  ip=require('os').networkInterfaces()["eth0"][0]["address"] ;
+  mac=require('os').networkInterfaces()["eth0"][0]["mac"];
+  
+
   res={"id":1,
 	"ip":ip,
         "mac":mac,
-	"names":list
+	"names":list()
       }
-
+console.log(list());
       return res;
 }
-console.log(getdoc());
+
 var request = require('request');
-var options = {
+
+setInterval(()=>{
+  var options = {
   'method': 'POST',
-  'url': 'http://192.168.43.155:8080/screen/sendMacIp',
+  'url': 'http://192.168.1.6:8080/screen/sendMacIp',
   'headers': {
     'Content-Type': 'application/json'
   },
   body: JSON.stringify(getdoc())
 
 };
-setInterval(()=>{
 request(options, function (error, response) {
   if (error) throw new Error(error);
   console.log(JSON.parse(response.body));
 
   const fs = require('fs');
-  fs.writeFile('orderMi/OrderMedia.json',ordonnance(JSON.parse(response.body)),err=>{
+  fs.readFile('./OrderMedia.js', 'utf8', function (err,data) {
+  if (err) {  return console.log(err);}
+  if(data!="json="+ordonnance(JSON.parse(response.body))){
+     fs.writeFile('./OrderMedia.js',"json="+ordonnance(JSON.parse(response.body)),err=>{
     if(err){
       console.error(err)
       return;
@@ -48,4 +58,21 @@ request(options, function (error, response) {
     
     
     });
+    var child_process = require("child_process");
+
+var task1 = child_process.exec("/usr/bin/chromium-browser --kiosk  --disable-restore-session-state file:///home/pi/Desktop/test.html", function (error, stdout, stderr) {
+console.log("stdout: " + stdout);
+console.log("stderr: " + stderr);
+if (error !== null) {
+console.log("exec error: " + error);
+}
+});
+    
+    
+    
+    
+  }
+});
+ 
 });},5000)
+
